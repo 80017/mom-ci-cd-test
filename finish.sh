18102325192a435ae64b7ac1c695cc7fbdd1d093
80017 <arpitap@officebeacon.com>
2018-01-11 13:01:27 +0530

    Update finish.sh

--- finish.sh
+++ finish.sh
@@ -7,7 +7,7 @@
 # 'http://rancher.flowz.com:8080/v2-beta/projects/1a29/services/1s302?action=finishupgrade'
 
 echo "call finish.sh file"
-echo ${env.BUILD_NUMBER}
+echo $COMMIT
 
 
-echo $env.BUILD_NUMBER
+
