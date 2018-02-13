bbe9fb97ec2d04a07a80afe625401ee0f39af02b
arpita patel <arpitap@officebeacon.com>
2018-02-13 10:49:12 +0530

    add script

--- finish.sh
+++ finish.sh
@@ -12,3 +12,26 @@ echo $TRAVIS_BUILD_NUMBER
 echo "TRAVIS_PULL_REQUEST is $TRAVIS_PULL_REQUEST"
 echo "TRAVIS_TAG is $TRAVIS_TAG"
 echo "TRAVIS_BUILD_NUMBER is $TRAVIS_BUILD_NUMBER"
+
+
+if [ "$TRAVIS_BRANCH" = "develop" ]; then
+
+  # Is this not a Pull Request?
+  if [ "$TRAVIS_PULL_REQUEST" = false ]; then
+
+    # Is this not a build which was triggered by setting a new tag?
+    if [ -z "$TRAVIS_TAG" ]; then
+      echo -e "Starting to tag commit.\n"
+
+      git config --global user.email "travis@travis-ci.org"
+      git config --global user.name "Travis"
+
+      # Add tag and push to master.
+      git tag -a v${TRAVIS_BUILD_NUMBER} -m "Travis build $TRAVIS_BUILD_NUMBER pushed a tag."
+      git push origin --tags
+      git fetch origin
+
+      echo -e "Done magic with tags.\n"
+  fi
+  fi
+fi
